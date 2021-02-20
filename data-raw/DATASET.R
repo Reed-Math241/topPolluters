6a4610a013567d2dc4e9f8285b803b5e24baf5ad
alyssandrichik <alyandric@reed.edu>
2021-02-20 14:42:48 -0800

    made percent variables' observations numeric and got rid of the percent sign

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,7 +1,6 @@
 ## code to prepare `DATASET` dataset goes here
-
-data <- read.csv("data.csv")
 library(dplyr)
+data <- read.csv("data.csv", na.strings=c(""))
 data <- data %>%
   rename(polluter = Parent.Entity.or.Corporation) %>%
   rename(toxic.air.rank = Toxic.100.Air.Rank) %>%
@@ -11,6 +10,9 @@ data <- data %>%
   rename(toxic.water.rank = Toxic.100.Water.Rank) %>%
   rename(greenhouse.poor = Greenhouse.100.EJ..Poor.Share) %>%
   rename(greenhouse.minority = Greenhouse.100.EJ..Minority.Share)
-
+data$toxic.air.poor = (as.numeric(sub("%", "", data$toxic.air.poor))/100)
+data$toxic.air.minority = (as.numeric(sub("%", "", data$toxic.air.minority))/100)
+data$greenhouse.poor = (as.numeric(sub("%", "", data$greenhouse.poor))/100)
+data$greenhouse.minority = (as.numeric(sub("%", "", data$greenhouse.minority))/100)
 usethis::use_data(data, overwrite = TRUE)
 
