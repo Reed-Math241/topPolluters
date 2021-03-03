3d9876639a362e42f44aff19c63d9526fc1c0106
albisbub <albisbub@reed.edu>
2021-03-02 18:43:56 -0800

    upppppandrunnnningnooeerrrrors

--- R/topPolluters.R
+++ R/topPolluters.R
@@ -1,6 +1,6 @@
 #' Top Climate, Air, and Water Polluters (2018)
-#' 
-#' Data on the combined toxic 100 / greenhouse 100 indexes (2020 report, based on 2018 data). Researchers at the Political Economy Research Institute (PERI) at the University of Massachusetts Amherst published new editions of the  Greenhouse 100  Index, ranking U.S. companies by their emissions responsible for global climate change according to the U.S. EPA Greenhouse Gas Reporting Program, and the Toxic 100 Air and Toxic 100 Water Indexes, ranking U.S. industrial polluters using the U.S. EPA Toxics Release Inventory. The PERI Indexes include Environmental Justice indicators to assess impacts on low-income people and minorities. 
+#'
+#' Data on the combined toxic 100 / greenhouse 100 indexes (2020 report, based on 2018 data). Researchers at the Political Economy Research Institute (PERI) at the University of Massachusetts Amherst published new editions of the  Greenhouse 100  Index, ranking U.S. companies by their emissions responsible for global climate change according to the U.S. EPA Greenhouse Gas Reporting Program, and the Toxic 100 Air and Toxic 100 Water Indexes, ranking U.S. industrial polluters using the U.S. EPA Toxics Release Inventory. The PERI Indexes include Environmental Justice indicators to assess impacts on low-income people and minorities.
 #'
 #' @format  A data frame with 220 observations on the following 8 variables:
 #' \describe{
@@ -14,4 +14,6 @@
 #'   \item{greenhouse.minority}{Greenhouse 100 EJ: Minority Share; shares of potential exposure to co-pollutants of combustion borne by people in minority racial/ethnic groups}
 #' }
 #' @source \url{https://www.peri.umass.edu/combined-toxic-100-greenhouse-100-indexes-current}
+#' @import dplyr
+
 "topPolluters"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,6 @@
 ## code to prepare `DATASET` dataset goes here
 library(dplyr)
-topPolluters <- read.csv("data.csv", na.strings=c(""))
+topPolluters <- read.csv("data.csv", na.strings = c(""))
 topPolluters <- topPolluters %>%
   rename(polluter = Parent.Entity.or.Corporation) %>%
   rename(toxic.air.rank = Toxic.100.Air.Rank) %>%
@@ -10,9 +10,8 @@ topPolluters <- topPolluters %>%
   rename(toxic.water.rank = Toxic.100.Water.Rank) %>%
   rename(greenhouse.poor = Greenhouse.100.EJ..Poor.Share) %>%
   rename(greenhouse.minority = Greenhouse.100.EJ..Minority.Share)
-topPolluters$toxic.air.poor = (as.numeric(sub("%", "", topPolluters$toxic.air.poor))/100)
-topPolluters$toxic.air.minority = (as.numeric(sub("%", "", topPolluters$toxic.air.minority))/100)
-topPolluters$greenhouse.poor = (as.numeric(sub("%", "", topPolluters$greenhouse.poor))/100)
-topPolluters$greenhouse.minority = (as.numeric(sub("%", "", topPolluters$greenhouse.minority))/100)
+topPolluters$toxic.air.poor <- (as.numeric(sub("%", "", topPolluters$toxic.air.poor)) / 100)
+topPolluters$toxic.air.minority <- (as.numeric(sub("%", "", topPolluters$toxic.air.minority)) / 100)
+topPolluters$greenhouse.poor <- (as.numeric(sub("%", "", topPolluters$greenhouse.poor)) / 100)
+topPolluters$greenhouse.minority <- (as.numeric(sub("%", "", topPolluters$greenhouse.minority)) / 100)
 usethis::use_data(topPolluters, overwrite = TRUE)
-
